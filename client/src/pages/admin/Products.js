4ae7119651b86569dd3ab46974040ebcf4144a2e
pravocodes <singhpraveen5673@gmail.com>
2023-08-06 23:44:25 +0530

    product update

--- client/src/pages/admin/Products.js
+++ client/src/pages/admin/Products.js
@@ -66,7 +66,7 @@ const Products = () => {
                     <div className="card-body">
                       <h5 className="card-title">{p.name}</h5>
                       <p className="card-text">{p.description}</p>
-                      <p className="card-text">₹{p.price}</p>
+                      <p className="card-text">${p.price}</p>
                       <p className="card-text">
                         Shipping: {p.shipping ? "Yes" : "No"}
                       </p>
